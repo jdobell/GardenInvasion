6ced370b04157888541a798e13657ef785f39a66
james <jdobell@gmail.com>
2015-11-08 21:35:21 -0800

    installed data encryption module and put in detection for device types

--- global-variables.lua
+++ global-variables.lua
@@ -1,5 +1,14 @@
 local configuration = {
-font = native.systemFont,
+-- Set up some defaults...
+isApple = false,
+isAndroid = false,
+isGoogle = false,
+isKindleFire = false,
+isNook = false,
+isIPad = false,
+isTall = false,
+isSimulator = false,
+font = native.systemFont
 }
 
 local localization = require( "mod_localize" )
@@ -17,6 +26,49 @@ end
 
 function configuration._s(string, ...)
 	return _string(string, ...)
+end
+
+local model = system.getInfo("model")
+
+-- Are we on the Simulator?
+if ( "simulator" == system.getInfo("environment") ) then
+    configuration.isSimulator = true
+end
+
+if ( (display.pixelHeight/display.pixelWidth) > 1.5 ) then
+   configuration.isTall = true
+end
+
+-- Now identify the Apple family of devices:
+if ( string.sub( model, 1, 2 ) == "iP" ) then 
+   -- We are an iOS device of some sort
+   configuration.isApple = true
+
+   if ( string.sub( model, 1, 4 ) == "iPad" ) then
+      configuration.is_iPad = true
+   end
+
+else
+
+	   -- Not Apple, so it must be one of the Android devices
+   configuration.isAndroid = true
+
+   -- Let's assume we are on Google Play for the moment
+   configuration.isGoogle = true
+
+   -- All of the Kindles start with "K", although Corona builds before #976 returned
+   -- "WFJWI" instead of "KFJWI" (this is now fixed, and our clause handles it regardless)
+   if ( model == "Kindle Fire" or model == "WFJWI" or string.sub( model, 1, 2 ) == "KF" ) then
+      configuration.isKindleFire = true
+      configuration.isGoogle = false  --revert Google Play to false
+   end
+
+   -- Are we on a Nook?
+   if ( string.sub( model, 1 ,4 ) == "Nook" or string.sub( model, 1, 4 ) == "BNRV" ) then
+      configuration.isNook = true
+      configuration.isGoogle = false  --revert Google Play to false
+   end
+
 end
 
 return configuration
